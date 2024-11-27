e9db5b6b36b7bbaf48d0c06b957d7e66097ad1d5
qioyqa <qioyqa@proton.me>
2024-11-27 22:47:09 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <filesystem>
 #include <conio.h>
+#include <stdio.h>
+#include <windows.h>
 namespace fs = std::filesystem;
 using namespace std;
 using namespace fs;
@@ -118,3 +120,119 @@ bool isTrue(vector<string> data, const string& login = "admin", const string& pa
         return false;
     }
 }
+
+bool isAuthificate(const string& login, const string& password) {
+    if (isRegistred(login)) {
+        vector<string> data = read_file(login);
+
+        if (isTrue(data, login, password)) {
+            cout << "> Welcome back, " << login << "!" << endl;
+            return true;
+        } else {
+            cout << "> Wrong password!" << endl;
+            return false;
+        }
+    } else {
+        auto file = create_file(login);
+        if (file.second) {
+            write_to_file(file.first, login, password);
+            cout << "> Dear " << login << ", thank you for registering!" << endl;
+            return true;
+        } else {
+            cerr << "> Error: Unable to create a file for the user." << endl;
+            return false;
+        }
+    }
+}
+
+bool isAdmin(const string& login, const string& password){
+    if (read_file(login)[0] == "admin" && password == "admin"){
+        cout << ">logined as admin " << endl;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void delete_user(const string& login){
+    if(isRegistred(login)){
+        string ffile = login + ".txt";
+        if (remove(ffile.c_str()) == 0){
+            cout << "User: " << login << " deleted successfully" << endl;
+        } else {
+            cout << "Can't delete this user!" << endl;
+        }
+    } else {
+        cout << "User not registered!" << endl;
+    }
+}
+
+void create_user(){
+    vector<string> user_data = get_data();
+    auto file = create_file(user_data[0]);
+    if (file.second){
+        write_to_file(user_data[0], user_data[0], user_data[1]);
+        cout << "User: " << user_data[0] << "created succesfully" << endl;
+    } else {
+        cout << "Can`t open file, try again." << endl;
+    }
+}
+
+void screen_clear(){
+    Sleep(100);
+    system("cls");
+}
+
+void print_table() {
+    cout << R"(
+            .___      .__                                    .__   
+_____     __| _/_____ |__| ____   ___________    ____   ____ |  |  
+\__  \   / __ |/     \|  |/    \  \____ \__  \  /    \_/ __ \|  |  
+ / __ \_/ /_/ |  Y Y  \  |   |  \ |  |_> > __ \|   |  \  ___/|  |__
+(____  /\____ |__|_|  /__|___|  / |   __(____  /___|  /\___  >____/
+     \/      \/     \/        \/  |__|       \/     \/     \/      
+    )" << endl;
+}
+
+void edit_password(const string& login) {
+    if (isRegistred(login)) {
+        vector<string> user_data = read_file(login);
+
+        cout << "> Current password required for " << login << ":" << endl;
+        string current_password = get_password();
+
+        if (isTrue(user_data, login, current_password)) {
+            cout << "> Enter new password: ";
+            string new_password = get_password();
+            cout << "> Confirm new password: ";
+            string confirm_password = get_password();
+
+            if (new_password == confirm_password) {
+                write_to_file(login, login, new_password);
+                cout << "> Password updated successfully for user: " << login << endl;
+            } else {
+                cout << "> Error: Passwords do not match. Try again." << endl;
+            }
+        } else {
+            cout << "> Error: Incorrect current password." << endl;
+        }
+    } else {
+        cout << "> Error: User " << login << " is not registered." << endl;
+    }
+}
+
+void admin_password(const string& login){
+    cout << "> Enter new password for user \"" << login << "\":" << endl;
+    string new_password = get_password();
+    cout << "> Confirm new password for user \"" << login << "\":" << endl;
+    string confirm_password = get_password();
+
+    if (new_password == confirm_password) {
+        write_to_file(login, login, new_password);
+        cout << "> Password updated successfully for user: " << login << endl;
+    } else {
+        cout << "> Error: Passwords do not match. Try again." << endl;
+    }
+}
+
+
